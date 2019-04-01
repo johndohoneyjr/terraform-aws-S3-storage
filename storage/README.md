10777d5faddb8d1bca47a01373bb8fb3af537c8b
John Dohoney Jr <johndohoneyjr@Blackhawk.fios-router.local>
2019-04-01 16:28:32 -0700

    test

---
+++ storage/README.md
@@ -0,0 +1,3 @@
+# terraform-aws-storage
+
+Storage Layer for S3
---
+++ storage/variables.tf
@@ -0,0 +1,40 @@
+variable "project_name" {}
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS profile used for provider connection"
+  default = "default"
+}
+
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-2e1ef954"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t2.micro"
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "Provisioned by Terraform"
+}
+
+variable "owner" {
+  description = "Name to pass to the Owner tag"
+  default = "Pizza-Pizza"
+}
+
+variable "ttl" {
+  description = "Hours until instances are reaped by N.E.P.T.R"
+  default = "3"
+}
+
+variable "description" {
+  description = "So meta"
+  default = "Foo"
+}
